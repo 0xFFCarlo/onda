6b1f0420baccda56e1eabb7d4125c59928dbd2a1
Flowx08 <meronicarlo98@gmail.com>
2026-03-10 14:57:18 +0100

    Added claude.md file

---
+++ src/onda_jit_x86_64.c
@@ -0,0 +1,13 @@
+#include "onda_jit_x86_64.h"
+
+size_t onda_jit_x86_64(const uint8_t* bytecode,
+                       const size_t bytecode_entry_pc,
+                       size_t bytecode_size,
+                       int64_t* data_sp,
+                       int64_t* frame_bp,
+                       uint8_t** out_machine_code,
+                       size_t* out_machine_code_size)
+{
+  // TODO:
+  return 0;
+}
